2f3cd0a416613f9cb49f6bd13220bd6be6349b79
CaioCDJ <caiodjesus1@hotmail.com>
2022-05-24 17:58:42 -0300

    Organizing Includes

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,26 +1,17 @@
 var conn = require('./../inc/db')
 var express = require('express');
+var menus = require('./../inc/menus');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title`,
-    (err,results)=>{
+  menus.getMenus().then(results=>{
 
-      if(err){
-      
-        console.log(err)
-      
-      } else{
-      
-        res.render('index', { 
-          title: 'Restaurante Saboroso!',
-          menus: results });
-
-      }
-    })  
+    res.render('index', { 
+      title: 'Restaurante Saboroso!',
+      menus: results });
+  })
 });
 
 router.get('/contacts', function(req,res,next){
@@ -35,12 +26,16 @@ router.get('/contacts', function(req,res,next){
 });
 router.get('/menus', function(req,res,next){
 
-  res.render('menus',{
+  menus.getMenus().then(results=>{
+    res.render('menus',{
     
-    title: 'Menu - Restaurante Saboroso!',
-    background:'images/img_bg_1.jpg',
-    h1:'Saboreie nosso menu!'
-  });
+      title: 'Menu - Restaurante Saboroso!',
+      background:'images/img_bg_1.jpg',
+      h1:'Saboreie nosso menu!',
+      menus:results
+      
+    });
+  })
 
 });
 router.get('/reservations', function(req,res,next){
